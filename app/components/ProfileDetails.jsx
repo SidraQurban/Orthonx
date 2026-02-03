90fd704f10041d884e633e2e30c2f32789229f8e
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2026-02-03 16:37:24 +0500

    feat(profile): update profile screen UI

---
+++ app/components/ProfileDetails.jsx
@@ -0,0 +1,161 @@
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from "react-native-responsive-dimensions";
+import { Ionicons, MaterialIcons, Feather } from "@expo/vector-icons";
+import MaterialDesignIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons";
+
+const ProfileDetails = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <View
+      style={{
+        marginHorizontal: responsiveHeight(1),
+      }}
+    >
+      {/* Name */}
+      <View style={{ marginTop: responsiveHeight(2) }}>
+        <Text>Full Name</Text>
+        <TextInput
+          placeholder="Sohail Ahmad"
+          style={{
+            marginTop: responsiveHeight(1),
+            borderWidth: 1,
+            borderColor: "#ced4da",
+            borderRadius: responsiveHeight(2),
+            paddingLeft: responsiveHeight(4),
+            backgroundColor: "#e9ecef",
+            height: responsiveHeight(5.5),
+            width: responsiveWidth(80),
+          }}
+        />
+        <MaterialIcons
+          name="person"
+          color="#6c757d"
+          size={20}
+          style={{
+            marginTop: responsiveHeight(-3.8),
+            marginLeft: responsiveWidth(2),
+          }}
+        />
+      </View>
+      {/* Email */}
+      <View style={{ marginTop: responsiveHeight(4) }}>
+        <Text>Email Address</Text>
+        <TextInput
+          placeholder="sohailahmad34280@gmail.com"
+          style={{
+            marginTop: responsiveHeight(1),
+            borderWidth: 1,
+            borderColor: "#ced4da",
+            borderRadius: responsiveHeight(2),
+            paddingLeft: responsiveHeight(4),
+            backgroundColor: "#e9ecef",
+            height: responsiveHeight(5.5),
+            width: responsiveWidth(80),
+          }}
+        />
+        <MaterialDesignIcons
+          name="email-outline"
+          color="#6c757d"
+          size={19}
+          style={{
+            marginTop: responsiveHeight(-3.8),
+            marginLeft: responsiveWidth(2),
+          }}
+        />
+        <Text
+          style={{
+            marginTop: responsiveHeight(1.5),
+            fontSize: responsiveFontSize(1.5),
+            color: "gray",
+          }}
+        >
+          Contact support to change email
+        </Text>
+      </View>
+      {/* Contact */}
+      <View style={{ marginTop: responsiveHeight(2) }}>
+        <Text>Phone Number</Text>
+        <TextInput
+          placeholder="+92 300 1234567"
+          style={{
+            marginTop: responsiveHeight(1),
+            borderWidth: 1,
+            borderColor: "#ced4da",
+            borderRadius: responsiveHeight(2),
+            paddingLeft: responsiveHeight(4),
+            backgroundColor: "#e9ecef",
+            height: responsiveHeight(5.5),
+            width: responsiveWidth(80),
+          }}
+        />
+        <Feather
+          name="phone"
+          color="#6c757d"
+          size={18}
+          style={{
+            marginTop: responsiveHeight(-4),
+            marginLeft: responsiveWidth(2),
+          }}
+        />
+        <View
+          style={{
+            marginTop: responsiveHeight(4),
+            marginHorizontal: responsiveHeight(1),
+            height: responsiveHeight(0.1),
+            backgroundColor: "lightgray",
+          }}
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: responsiveHeight(2),
+        }}
+      >
+        <SimpleLineIcons name="lock" color="#00B4D8" size={18} />
+        <Text
+          style={{
+            fontSize: responsiveFontSize(2),
+            fontWeight: "bold",
+            marginLeft: responsiveHeight(1),
+          }}
+        >
+          Security
+        </Text>
+      </View>
+      <View style={{ marginTop: responsiveHeight(1) }}>
+        <Text>New Password</Text>
+        <TextInput
+          placeholder="Leave empty to keep current "
+          style={{
+            marginTop: responsiveHeight(1),
+            borderWidth: 1,
+            borderColor: "#ced4da",
+            borderRadius: responsiveHeight(2),
+            paddingLeft: responsiveHeight(4),
+            backgroundColor: "#e9ecef",
+            height: responsiveHeight(5.5),
+            width: responsiveWidth(80),
+          }}
+        />
+        <SimpleLineIcons
+          name="lock"
+          color="#6c757d"
+          size={18}
+          style={{
+            marginTop: responsiveHeight(-4),
+            marginLeft: responsiveWidth(2),
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default ProfileDetails;
--- app/screens/Profile.jsx
+++ app/screens/Profile.jsx
@@ -1,11 +1,69 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground, TouchableOpacity } from "react-native";
 import React from "react";
+import {
+  responsiveFontSize,
+  responsiveHeight,
+} from "react-native-responsive-dimensions";
+import { Image } from "react-native";
+import ProfileDetails from "../components/ProfileDetails";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <ImageBackground
+      source={require("../../assets/bgimg.png")}
+      style={{ flex: 1 }}
+      resizeMode="cover"
+    >
+      <View
+        style={{
+          height: responsiveHeight(70),
+          width: responsiveHeight(40),
+          backgroundColor: "white",
+          margin: 20,
+          borderRadius: responsiveHeight(2),
+        }}
+      >
+        <View
+          style={{
+            marginHorizontal: responsiveHeight(1),
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity style={{ marginTop: responsiveHeight(1) }}>
+            <Image
+              source={require("../../assets/profile-pic.png")}
+              style={{
+                width: 100,
+                height: 100,
+                height: responsiveHeight(10),
+                width: responsiveHeight(10),
+                borderRadius: responsiveHeight(5),
+                backgroundColor: "gray",
+              }}
+            />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: responsiveFontSize(1.7),
+              marginTop: responsiveHeight(4),
+              marginLeft: responsiveHeight(2),
+              fontWeight: "bold",
+            }}
+          >
+            sohailahmad34280
+          </Text>
+        </View>
+        <View
+          style={{
+            marginTop: responsiveHeight(1),
+            marginHorizontal: responsiveHeight(1),
+            height: responsiveHeight(0.1),
+            backgroundColor: "lightgray",
+          }}
+        />
+        <ProfileDetails />
+      </View>
+    </ImageBackground>
   );
 };
 

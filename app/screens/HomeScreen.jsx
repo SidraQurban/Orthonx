7fd3b32c99cdc9ad375f6330263bd7de302fe493
Sidra Qurban <100536445+SidraQurban@users.noreply.github.com>
2026-02-04 21:21:31 +0500

    refactor(home): update HomeScreen

--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -1,10 +1,12 @@
-import { ImageBackground } from "react-native";
+import { ImageBackground, View } from "react-native";
 import React from "react";
 import ImageSlider from "../components/ImageSlider";
 import Cards from "../components/Cards";
 import Recommendations from "../components/Recommendations";
 import { FlatList } from "react-native";
 import Chatbot from "../components/Chatbot";
+import ReportActivity from "../components/ReportActivity";
+import { responsiveHeight } from "react-native-responsive-dimensions";
 
 const HomeScreen = () => {
   return (
@@ -15,7 +17,11 @@ const HomeScreen = () => {
     >
       <FlatList
         data={[1]} // dummy
-        renderItem={() => <Recommendations />}
+        renderItem={() => (
+          <View style={{ paddingTop: responsiveHeight(2) }}>
+            <ReportActivity />
+          </View>
+        )}
         keyExtractor={(item) => item.toString()}
         ListHeaderComponent={
           <>
